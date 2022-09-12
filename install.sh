6ca57bdfb7601aa01b88cf673e71e0d19378eb52
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-09-12 19:00:54 +0900

    Fix

--- install.sh
+++ install.sh
@@ -3,33 +3,33 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # bashrc
 if [ -f ~/.bashrc ]; then
-    echo -n "setting config for bash"
+    echo -n "setting config for bash... "
     echo "source $SCRIPT_DIR/aliases" > ~/.bash_aliases
     echo "# auto start tmux" >> ~/.bashrc
     echo "source $SCRIPT_DIR/auto_tmux" >> ~/.bashrc
-    echo " ... Done!"
+    echo "Done"
 fi
 
 
 # zshrc
 if [ -f ~/.zshrc ]; then
-    echo -n "setting config for zsh"
+    echo -n "setting config for zsh... "
     echo "# Set aliases" >> ~/.zshrc
     echo "source $SCRIPT_DIR/aliases" >> ~/.zshrc
     echo "# auto start tmux" >> ~/.zshrc
     echo "source $SCRIPT_DIR/auto_tmux" >> ~/.zshrc
-    echo " ... Done!"
+    echo "Done"
 fi
 
 
 # tmux
-echo -n "setting tmux"
+echo -n "setting tmux... "
 echo "source $SCRIPT_DIR/tmux.conf" > ~/.tmux.conf
-echo " ... Done!"
+echo "Done"
 
 
 # vim
-echo "setting vim"
+echo "setting vim... "
 echo "source $SCRIPT_DIR/vimrc" > ~/.vimrc
 
 if [ -d ~/.vim/undo ]; then
