fc9a70f7e292d1a89c6aed1f820a07f938c3d14e
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-07-24 20:07:38 +0900

    Fix

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # git
-echo "setting git... "
+echo -n "setting git... "
 if [ -e ~/.gitconfig ]; then
     cat $SCRIPT_DIR/gitconfig >> ~/.gitconfig
 else
     cat $SCRIPT_DIR/gitconfig > ~/.gitconfig
 fi
-echo ">>> Done"
+echo "Done"
 echo ""
 
 # shell, tmux, vim, neovim
