dfc335215297296321b78f0956446986f93de56c
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-08-05 18:17:18 +0900

    Remove private setting files

--- scripts/auto_tmux.sh
+++
@@ -1,9 +0,0 @@
-# auto start tmux
-if [ $SHLVL = 1 ]; then
-    TMUX_COUNT=$(ps -ax | grep '[t]mux' | wc -l)
-    if [ $TMUX_COUNT -eq 0 ]; then
-        tmux
-    elif [ $TMUX_COUNT ]; then
-        tmux a
-    fi
-fi
--- scripts/load_private_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TARGET_DIR=$HOME/dotfiles/scripts
-
-source $TARGET_DIR/auto_tmux.sh
-source $TARGET_DIR/rosconfig.sh
