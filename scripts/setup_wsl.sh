53aafe2f3d50c4551c106ff61950a6b816cfed2b
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-08-28 23:17:11 +0900

    Update setup_wsl.sh

--- scripts/setup_wsl.sh
+++ scripts/setup_wsl.sh
@@ -3,3 +3,7 @@
 sudo apt -y install language-pack-ja
 sudo update-locale LANG=ja_JP.UTF8
 sudo apt -y install manpages-ja manpages-ja-dev
+
+# Set display settings
+echo "export DISPLAY=\$(cat /etc/resolv.conf | grep nameserver | awk '{print \$2}'):0" >> ~/.bashrc
+echo "export LIBGL_ALWAYS_SOFTWARE=1" >> ~/.bashrc
