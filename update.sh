3f68690d340492ae5d076665ef67e20da6e763fe
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-09-24 22:27:20 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,12 +12,16 @@ echo -n "setting shell... "
 if [ -e ~/.bashrc ]; then # bashrc
     LOAD_COMMAND_COUNT=$(cat ~/.bashrc | grep dotfiles\/scripts\/load_files\.sh | wc -l)
     if [ $LOAD_COMMAND_COUNT -eq 0 ]; then
+        echo "" >> ~/.bashrc
+        echo "# dotfiles" >> ~/.bashrc
         echo "source $SCRIPT_DIR/scripts/load_files.sh" >> ~/.bashrc
     fi
 fi
 if [ -e ~/.zshrc ]; then # zshrc
     LOAD_COMMAND_COUNT=$(cat ~/.zshrc | grep dotfiles\/scripts\/load_files\.sh | wc -l)
     if [ $LOAD_COMMAND_COUNT -eq 0 ]; then
+        echo "" >> ~/.bashrc
+        echo "# dotfiles" >> ~/.bashrc
         echo "source $SCRIPT_DIR/scripts/load_files.sh" >> ~/.zshrc
     fi
 fi
