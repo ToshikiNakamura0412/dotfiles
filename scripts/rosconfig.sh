1b02619af488e89472ed681f59f05405a3763ab3
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-01-10 13:28:25 +0900

    Update rosconfig.sh

--- scripts/rosconfig.sh
+++ scripts/rosconfig.sh
@@ -1,10 +1,8 @@
 ROS_DISTRO=noetic
+ROS_WORKSPACE=~/catkin_ws
 
 # Set basic ROS conf
-export ROS_WORKSPACE=$HOME/catkin_ws
-export ROS_PACKAGE_PATH=$ROS_WORKSPACE/src:$ROS_PACKAGE_PATH
 export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
-export EDITOR='vim'
 if [ $0 = 'bash' ]; then # bash
     source /opt/ros/$ROS_DISTRO/setup.bash
     source $ROS_WORKSPACE/devel/setup.bash
@@ -13,28 +11,28 @@ else # zsh
     source $ROS_WORKSPACE/devel/setup.zsh
 fi
 
-# Set ROS alias command
+# Set basic alias commands
 alias cw='cd $ROS_WORKSPACE'
 alias cs='cd $ROS_WORKSPACE/src'
-alias cb='catkin build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
-alias cbt='catkin build --this --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+alias cb='catkin build -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+alias cbt='catkin build --this -DCMAKE_BUILD_TYPE=RelWithDebInfo'
 
-# Set compile command for ROS completion
-function ln-compile-commands-json() {
-    # Find compile_commands.json in build directory and create symlink to the top of the package directories.
-    local package_xmls=$(find $ROS_WORKSPACE/src -name package.xml)
-    for package_xml in $(echo $package_xmls); do
-        local package_name=$(cat $package_xml | grep '<name>' | sed 's/ //g' | sed s/name//g | sed s/\<//g | sed s/\>//g | sed 's/\///g')
+# Set alias commands for ROS completion and build
+function create-simple-compile-flags() {
+    echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
+    echo "-I$PWD/include" >> compile_flags.txt
+}
+function cbt-export-compile-commands() {
+    catkin build --this -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+    echo ""
+    if [ -e package.xml ]; then
+        local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
         if [ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]; then
-            ln -sf $ROS_WORKSPACE/build/$package_name/compile_commands.json $(rospack find $package_name)/compile_commands.json
+            ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
+        else
+            echo "Error: compile_commands.json not found in build directory"
         fi
-    done
-}
-function catkin-compile-commands-json() {
-    cd $ROS_WORKSPACE && catkin build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-    ln-compile-commands-json
-}
-function catkin-compile-commands-json-this() {
-    catkin build --this --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-    ln-compile-commands-json
+    else
+        echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
+    fi
 }
