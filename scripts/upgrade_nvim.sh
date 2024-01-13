08e6b6a158a1a085d162cd3c2f467062aff7998c
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-13 13:16:18 +0900

    Update upgrade_nvim.sh (#77)

--- scripts/upgrade_nvim.sh
+++ scripts/upgrade_nvim.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+# libfuse2
+source /etc/os-release
+OS_NAME=$(echo $ID$VERSION_ID | cut -d '.' -f 1)
+if [ $OS_NAME = "ubuntu22" ]; then
+    echo "============================"
+    echo " libfuse2 will be installed"
+    echo "============================"
+    sudo apt install -y --no-install-recommends libfuse2
+    echo ">>> Done"
+    echo ""
+fi
+
 # neovim
 echo "========================================"
 echo " neovim will be installed and upgraded"
@@ -9,7 +21,7 @@ sudo wget -O /opt/nvim.appimage https://github.com/neovim/neovim/releases/downlo
 sudo chmod +x /opt/nvim.appimage
 sudo ln -sf /opt/nvim.appimage /usr/bin/vim
 sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
-sudo apt install xsel -y --no-install-recommends
+sudo apt install -y --no-install-recommends xsel
 echo ">>> Done"
 echo ""
 
