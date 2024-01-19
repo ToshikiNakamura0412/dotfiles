5f7332c57b8a9da241c5c159c88675db8aaeb6fb
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-20 00:57:18 +0900

    Remove nvim (#78)
    * Remove nvim
    
    * Remove some files related to nvim

--- nvim/init.vim
+++
@@ -1,5 +0,0 @@
-source ~/.config/nvim/basic-settings.vim
-source ~/.config/nvim/keymap.vim
-source ~/.config/nvim/plugin.vim
-source ~/.config/nvim/plugin-settings.vim
-source ~/.config/nvim/plugin-keymap.vim
--- scripts/setup_fzf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# ripgrep
-echo "==========================="
-echo " ripgrep will be installed"
-echo "==========================="
-sudo apt install ripgrep npm -y --no-install-recommends
-echo ">>> Done"
-echo ""
