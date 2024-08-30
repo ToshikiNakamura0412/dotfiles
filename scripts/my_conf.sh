a8f3354e4505de7c6fbe39a2178130e280a201b3
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-08-30 11:59:25 +0900

    Update alias commands (#106)

--- scripts/my_conf.sh
+++ scripts/my_conf.sh
@@ -1,60 +1,100 @@
-#!/bin/sh
+source /etc/os-release
+OS_NAME=$(echo $ID)
 
-# =========
-# Git Conf
-# =========
-if [ $0 = "bash" ]; then
+if [[ $OS_NAME = "alpine" ]]; then
+  alias ll='ls -l'
+  alias la='ls -lA'
+  alias l='ls -la'
+
+elif [[ $OS_NAME = "ubuntu" ]]; then
+  # Show git branch
+  if [[ $0 = "bash" ]]; then
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1)\$ '
-fi
-
-# =========
-# ROS Conf
-# =========
-if [ -d /opt/ros ]; then
-  ROS_DISTRO=noetic
-  ROS_WORKSPACE=~/catkin_ws
-
-  # Set basic ROS conf
-  export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
-  if [ $0 = 'bash' ]; then # bash
-      source /opt/ros/$ROS_DISTRO/setup.bash
-      source $ROS_WORKSPACE/devel/setup.bash
-  else # zsh
-      source /opt/ros/$ROS_DISTRO/setup.zsh
-      source $ROS_WORKSPACE/devel/setup.zsh
   fi
 
-  # Set basic alias commands
-  alias cw='cd $ROS_WORKSPACE'
-  alias cs='cd $ROS_WORKSPACE/src'
-  alias cb='catkin build -DCMAKE_BUILD_TYPE=RelWithDebInfo'
-  alias cbt='catkin build --this -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+  # Set alias commands for ROS
+  if [[ -d /opt/ros ]]; then
+    alias cw='cd $ROS_WORKSPACE'
+    alias cs='cd $ROS_WORKSPACE/src'
 
-  # Set alias commands for ROS completion and build
-  function create_simple_compile_flags() {
-      echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
-      echo "-I$PWD/include" >> compile_flags.txt
-  }
-  function cbt_export_compile_commands() {
-      catkin build --this -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-      echo ""
-      if [ -e package.xml ]; then
+    if [[ $ROS_VERSION -eq 1 ]]; then
+      alias cb='catkin build -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+      alias cbt='catkin build --this -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+
+      # Set alias commands for ROS completion and build
+      function create_simple_compile_flags() {
+        echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
+        echo "-I$PWD/include" >> compile_flags.txt
+      }
+      function cbt_export_compile_commands() {
+        catkin build --this -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+        echo ""
+        if [[ -e package.xml ]]; then
           local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
-          if [ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]; then
-              ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
+          if [[ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]]; then
+            ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
           else
-              echo "Error: compile_commands.json not found in build directory"
+            echo "Error: compile_commands.json not found in build directory"
           fi
-      else
+        else
           echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
+        fi
+      }
+
+    else
+      # Set alias commands for build
+      function cb() {
+        local pwd=$(pwd)
+        cd $ROS_WORKSPACE
+        colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+        cd $pwd
+      }
+      function cbt() {
+        if [[ -e package.xml ]]; then
+          local pwd=$(pwd)
+          local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
+          cd $ROS_WORKSPACE
+          colcon build --packages-select $package_name --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+          cd $pwd
+        else
+          echo "Error: package.xml not found in current directory, so can't build a package"
+        fi
+      }
+
+      # Set alias commands for clean workspace
+      function colcon_clean() {
+        echo -n "rm: remove directories 'build' 'install' 'log'? (yes/no) "
+        read str
+        if [[ $str = "yes" ]]; then
+          local pwd=$(pwd)
+          cd $ROS_WORKSPACE
+          rm -rf build install log
+          cd $pwd
+        fi
+        unset AMENT_PREFIX_PATH
+        unset CMAKE_PREFIX_PATH
+        if [[ $0 = "bash" ]]; then
+          source /opt/ros/humble/setup.bash
+        else
+          source /opt/ros/humble/setup.zsh
+        fi
+      }
+
+      # Enable tab completion of colcon_cd
+      if [[ $0 = "bash" ]]; then
+        function _colcon_cd_packages() {
+          local cur=${COMP_WORDS[COMP_CWORD]}
+          local packages=$(cd && colcon list -n)
+          COMPREPLY=($(compgen -W "$packages" -- "$cur"))
+        }
+        complete -F _colcon_cd_packages colcon_cd
+      else
+        function _colcon_cd_packages() {
+          local packages=$(cd && colcon list -n)
+          reply=(${(ps:\n:)packages})
+        }
+        compctl -K _colcon_cd_packages colcon_cd
       fi
-  }
-elif [ -d ~/ros1_ws ]; then
-  ROS_WORKSPACE=~/ros1_ws
-  alias cw='cd $ROS_WORKSPACE'
-  alias cs='cd $ROS_WORKSPACE/src'
-elif [ -d ~/ros2_ws ]; then
-  ROS_WORKSPACE=~/ros2_ws
-  alias cw='cd $ROS_WORKSPACE'
-  alias cs='cd $ROS_WORKSPACE/src'
+    fi
+  fi
 fi
