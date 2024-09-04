75238ca9af7924b4cd1d31248bd0a30d5aa180d4
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-09-04 17:52:38 +0900

    Update PS1 for bash

--- scripts/my_conf.sh
+++ scripts/my_conf.sh
@@ -7,7 +7,11 @@ fi
 
 # Show git branch
 if [[ -n "$BASH_VERSION" ]]; then
-  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1)\$ '
+  if ! type __git_ps1 &> /dev/null; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  else
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1)\$ '
+  fi
 fi
 
 if [[ $OS_NAME == "alpine" ]]; then
