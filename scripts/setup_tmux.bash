44f298099cabe97aa6cd40d539c1f84f55e0993b
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-09-05 22:21:52 +0900

    Update setup_tmux.bash

--- scripts/setup_tmux.bash
+++ scripts/setup_tmux.bash
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-VALID_TMUX_POWER_THEME=("default" "gold" "everforest" "moon" "coral" "snow" "forest" "violet" "redwine")
+VALID_TMUX_POWER_THEME=(
+    "default"
+    "gold"
+    "everforest"
+    "moon"
+    "coral"
+    "snow"
+    "forest"
+    "violet"
+    "redwine"
+)
 
 function show_usage() {
     echo ""
-    echo "Usage: $0 <prefix_key> <tmux_power_theme>"
+    echo "Usage:"
+    echo "    Setup tmux:"
+    echo "        $0"
     echo ""
-    echo "    <prefix_key> : tmux prefix key (e.g. C-s, C-a)"
-    echo "    <tmux_power_theme> : theme name of tmux-power"
+    echo "    Change tmux prefix key and tmux-power theme:"
+    echo "        $0 <prefix_key> <tmux_power_theme> [with_setup]"
+    echo ""
+    echo "        <prefix_key> : tmux prefix key (e.g. C-s, C-a)"
+    echo "        <tmux_power_theme> : theme name of tmux-power"
     for theme in ${VALID_TMUX_POWER_THEME[@]}; do
-        echo "        - $theme"
+        echo "            - $theme"
     done
+    echo "        [with_setup] : setup tmux (optional)"
 }
 
 function setup_tmux() {
@@ -27,7 +43,7 @@ function setup_tmux() {
     echo ""
 }
 
-function check_valid_tmux_power_theme() {
+function is_valid_tmux_power_theme() {
     local tmux_power_theme=$1
     for theme in ${VALID_TMUX_POWER_THEME[@]}; do
         if [[ $tmux_power_theme == $theme ]]; then
@@ -41,32 +57,39 @@ function change_tmux_conf() {
     local prefix_key=$1
     local tmux_power_theme=$2
 
-    if ! check_valid_tmux_power_theme $tmux_power_theme; then
+    if ! is_valid_tmux_power_theme $tmux_power_theme; then
         echo "Error: invalid tmux power theme"
         show_usage
         exit 1
     fi
 
+    echo ""
+    echo "changing tmux conf... "
     echo "prefix key: $prefix_key"
     echo "tmux power theme: $tmux_power_theme"
 
-    if [[ ! -e $HOME/.tmux.conf ]]; then
+    if [[ ! -e ~/.tmux.conf ]]; then
         ln -sfv ${SCRIPT_DIR}/../tmux.conf ~/.tmux.conf
     fi
     if [[ -e ../tmux.conf.bak ]]; then
         cp ../tmux.conf.bak ../tmux.conf
     fi
-    sed -i.bak "s/set -g prefix C-s/set -g prefix $prefix_key/g" $(readlink -f $HOME/.tmux.conf)
-    sed -i "s/set -g @tmux_power_theme 'moon'/set -g @tmux_power_theme '$tmux_power_theme'/g" $(readlink -f $HOME/.tmux.conf)
+    sed -i.bak "s/set -g prefix C-s/set -g prefix $prefix_key/g" $(readlink -f ~/.tmux.conf)
+    sed -i "s/set -g @tmux_power_theme 'moon'/set -g @tmux_power_theme '$tmux_power_theme'/g" $(readlink -f ~/.tmux.conf)
+    echo ">>> Done"
 }
 
 function main() {
-    if [[ $1 == "-h" ]] || [[ $# -ne 2 ]]; then
+    if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
         show_usage
         exit 0
     fi
-
-    change_tmux_conf $1 $2
+    if [[ $# -ge 2 ]]; then
+        change_tmux_conf $1 $2
+    fi
+    if [[ $# -eq 0 ]] || [[ $3 == "with_setup" ]]; then
+        setup_tmux
+    fi
 }
 
 main $@
