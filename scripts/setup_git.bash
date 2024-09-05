37f2ea1d5b55f297443527ad17ce257110620941
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-09-05 20:05:57 +0900

    Fix format of setup_git.bash

--- scripts/setup_git.bash
+++ scripts/setup_git.bash
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-if [ "$(uname)" = "Linux" ]; then
-    source /etc/os-release
-    OS_NAME=$(echo $ID)
-elif [ "$(uname)" = "Darwin" ]; then
-    OS_NAME="mac"
-else
-    echo "This OS is not supported."
-    exit 1
-fi
+source $SCRIPT_DIR/common.bash
 
-echo ""
-echo "setting git... "
-if [ -e ~/.gitconfig ]; then
-    SETTING_COUNT1=$(cat ~/.gitconfig | grep alias | wc -l)
-    # SETTING_COUNT2=$(cat ~/.gitconfig | grep url | wc -l)
-    # if [ $SETTING_COUNT1 -eq 0 ] || [ $SETTING_COUNT2 -eq 0 ]; then
-    if [ $SETTING_COUNT1 -eq 0 ]; then
-        cat $SCRIPT_DIR/gitconfig >> ~/.gitconfig
+function setup_git() {
+    local distro=$(get_distro)
+
+    echo ""
+    echo "setting git... "
+    if [[ -e ~/.gitconfig ]]; then
+        if ! grep -Fq "alias" ~/.gitconfig; then
+            cat $SCRIPT_DIR/gitconfig >> ~/.gitconfig
+        fi
+    else
+        cat $SCRIPT_DIR/gitconfig > ~/.gitconfig
     fi
-else
-    cat $SCRIPT_DIR/gitconfig > ~/.gitconfig
-fi
-if [ ! -d ~/.config/git ]; then
-    mkdir -p -v ~/.config/git
-fi
-if [ $OS_NAME = "ubuntu" ] || [ $OS_NAME = "debian" ] || [ $OS_NAME = "fedora" ] || [ $OS_NAME = "opensuse-leap" ] || [ $OS_NAME = "mac" ]; then
-    ln -siv $SCRIPT_DIR/gitignore ~/.config/git/ignore
-elif [ $OS_NAME = "alpine" ]; then
-    ln -sv $SCRIPT_DIR/gitignore ~/.config/git/ignore
-fi
-echo ">>> Done"
-echo ""
+    if [[ ! -d ~/.config/git ]]; then
+        mkdir -p -v ~/.config/git
+    fi
+    if [[ ${distro} == "alpine" ]]; then
+        ln -sv $SCRIPT_DIR/gitignore ~/.config/git/ignore
+    elif is_valid_distro ${distro}; then
+        ln -siv $SCRIPT_DIR/gitignore ~/.config/git/ignore
+    fi
+    echo ">>> Done"
+    echo ""
+}
+
+function main() {
+    check_os
+    setup_git
+}
+
+main
