b30ee36e8564ab0f46e3e83343a079a689546b43
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-11-22 00:13:27 +0900

    Update ROS config

--- scripts/my_conf.bash
+++ scripts/my_conf.bash
@@ -31,27 +31,28 @@ elif [[ ${DISTRO} == "ubuntu" ]]; then
     alias cs='cd $ROS_WORKSPACE/src'
 
     if [[ $ROS_VERSION -eq 1 ]]; then
-      alias cb='catkin build -DCMAKE_BUILD_TYPE=RelWithDebInfo'
-      alias cbt='catkin build --this -DCMAKE_BUILD_TYPE=RelWithDebInfo'
-
       # Set alias commands for ROS completion and build
       function create_simple_compile_flags() {
-        echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
-        echo "-I$PWD/include" >> compile_flags.txt
+        echo "-I/opt/ros/${ROS_DISTRO}/include" > compile_flags.txt
+        echo "-I${PWD}/include" >> compile_flags.txt
       }
-      function cbt_export_compile_commands() {
-        catkin build --this -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-        echo ""
-        if [[ -e package.xml ]]; then
-          local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
-          if [[ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]]; then
-            ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
-          else
-            echo "Error: compile_commands.json not found in build directory"
+      function ln-compile-commands-json() {
+        # Find compile_commands.json in build directory and create symlink to the top of the package directories.
+        local package_xmls=$(find ${ROS_WORKSPACE}/src -name package.xml)
+        for package_xml in $(echo ${package_xmls}); do
+          local package_name=$(cat ${package_xml} | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
+          if [[ -e ${ROS_WORKSPACE}/build/${package_name}/compile_commands.json ]]; then
+            ln -sf ${ROS_WORKSPACE}/build/${package_name}/compile_commands.json $(rospack find ${package_name})/compile_commands.json
           fi
-        else
-          echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
-        fi
+        done
+      }
+      function cb() {
+        catkin build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+        ln-compile-commands-json
+      }
+      function cbt() {
+        catkin build --this --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+        ln-compile-commands-json
       }
 
     else
