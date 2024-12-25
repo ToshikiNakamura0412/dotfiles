eca6622beadbb2b81782937ac94d931e53c3ff25
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-12-25 12:13:22 +0900

    Update install script (#140)

--- install.bash
+++ install.bash
@@ -3,30 +3,33 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 function install_prerequisites() {
-    echo ""
-    echo "======================="
-    echo " install prerequisites"
-    echo "======================="
-    echo ""
-    ${SCRIPT_DIR}/scripts/install_prerequisites.bash
+  echo ""
+  echo "======================="
+  echo " install prerequisites"
+  echo "======================="
+  echo ""
+  ${SCRIPT_DIR}/scripts/install_prerequisites.bash
 }
 
 function setup() {
-    echo ""
-    echo "======="
-    echo " setup"
-    echo "======="
-    echo ""
-    ${SCRIPT_DIR}/scripts/setup_git.bash
-    ${SCRIPT_DIR}/scripts/setup_shell.bash
-    ${SCRIPT_DIR}/scripts/setup_tmux.bash
+  echo ""
+  echo "======="
+  echo " setup"
+  echo "======="
+  echo ""
+  ${SCRIPT_DIR}/scripts/setup_git.bash
+  ${SCRIPT_DIR}/scripts/setup_shell.bash
+  ${SCRIPT_DIR}/scripts/setup_tmux.bash
+  if [[ -e /usr/bin/nvim ]]; then
+    sudo ln -sfv /usr/bin/nvim /usr/bin/vim
+  fi
 }
 
 function main() {
-    install_prerequisites
-    setup
-    echo ""
-    echo "please set your terminal font as 'Hack Nerd Font Regular'"
+  install_prerequisites
+  setup
+  echo ""
+  echo "please set your terminal font as 'Hack Nerd Font Regular'"
 }
 
 main
